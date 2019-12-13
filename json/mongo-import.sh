16d0e08def45057f9e030d0901d317318aa7b082
Jeff Cogswell <jeffcogs@gmail.com>
2019-12-12 20:03:41 -0500

    Converted to JSON to support nulls

--- json/mongo-import.sh
+++ json/mongo-import.sh
@@ -1,7 +1,7 @@
-for f in *.csv
+for f in *.json
 do
     filename=$(basename "$f")
     extension="${filename##*.}"
     filename="${filename%.*}"
-    mongoimport -d ClassicModels -c "$filename" --type csv --file "$f" --headerline
+    mongoimport -d ClassicModels -c "$filename" --type json --file "$f"
 done
